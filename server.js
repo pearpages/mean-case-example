4f1cc825b7366959322da0cb9521ca5fe776796b
pearpages <pere@soms.cat>
2016-01-03 18:23:55 +0100

    mongo setup

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var app = express();
 var stylus = require('stylus');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var port = 3030;
 
 setViews();
-setMiddleware(); //because we sue static files, it's important this comes before the router
+setMiddleware(); //because we use static files, it's important this comes before the router
 settingRoutes();
+mongoInit();
 
 app.listen(port);
 console.log('Listening on port ' + port + '...');
@@ -43,4 +45,13 @@ function setMiddleware() {
     function compile(str, path) {
         return stylus(str).set('filename', path);
     }
+}
+
+function mongoInit() {
+    mongoose.connect('mongodb://localhost/multivision');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error...'));
+    db.once('open', function callback() {
+        console.log('multivision db opened');
+    });
 }
